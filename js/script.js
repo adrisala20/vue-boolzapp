56c91ce147ac1d952db97136af703ac420363308
adrisala20 <adriana_15_tuya@hotmail.com>
2024-04-04 14:46:33 +0200

    added method clickActiveContact

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
 import { contacts } from "./data.js";
+
+//const dt = luxon.DateTime;
+
 const{createApp}= Vue;
 createApp ({
     data(){
         return{
             contacts,
-            activeContactId: 1
+            activeContactId: 1,
         }
     },
     methods:{
-
+       clickActiveContact(id){
+        this.activeContactId = id;
+       },
     },
     computed:{
         activeContact(){
             return this.contacts.find((el)=> el.id ===this.activeContactId)
-        }
+        }, 
     },
     mounted(){
         console.log(this.contacts)
     }
 
 }).mount('#app')
+
